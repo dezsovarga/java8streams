da2212d8e698c925da479a44514e07984a5438ba
Dezso Varga <softdvarga@alarm.com>
2018-09-04 19:07:13 +0300

    added missing numbers exercise

--- src/test/java/com/dezso/varga/exercise/coding/MissingNumbersSpec.java
+++ src/test/java/com/dezso/varga/exercise/coding/MissingNumbersSpec.java
@@ -9,16 +9,15 @@ import java.util.List;
 import static com.dezso.varga.exercise.coding.MissingNumbers.*;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MissingNumbersSpec {
 
     @Test
     public void transformShouldConvertCollectionElementsToUpperCase() {
         int[] numbers = new int[]{1, 2, 3, 4, 5, 7, 8, 10};
-        int expectedMissingNumber = 6;
+        List<Integer> expectedMissingNumbers = Arrays.asList(6,9);
         List<Integer> actualMissingNumbers = getMissingNumbers(numbers, 10);
-        Assert.assertTrue(Arrays.asList(expectedMissingNumber).containsAll(actualMissingNumbers) );
-        Assert.assertTrue(actualMissingNumbers.containsAll(Arrays.asList(expectedMissingNumber)));
-
+        assertEquals(expectedMissingNumbers, actualMissingNumbers);
     }
 }
